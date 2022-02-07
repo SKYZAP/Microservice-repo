6d5c1382cd0964e2e25c76a953c0b27cf0fa2b67
SKYZAP <izzat.jehan@gmail.com>
2022-02-07 13:14:25 +0800

    Added more unit tests

--- main/src/app.controller.spec.ts
+++ main/src/app.controller.spec.ts
@@ -29,6 +29,50 @@ describe('AppController', () => {
         updatedAt: Date.now(),
       };
     }),
+    findAllOrder: jest.fn(() => {
+      return [
+        {
+          id: 3230240242042,
+          userId: '012220540493',
+          productName: 'RON96',
+          productPrice: 10,
+          productQuantity: 5,
+          totalCost: '50',
+          orderState: OrderStatusI.CREATED,
+          createdAt: Date.now(),
+          updatedAt: Date.now(),
+        },
+      ];
+    }),
+    findOneOrder: jest.fn(() => {
+      return {
+        id: 120122121313,
+        userId: '012220540493',
+        productName: 'RON96',
+        productPrice: 10,
+        productQuantity: 5,
+        totalCost: '50',
+        orderState: OrderStatusI.CREATED,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+      };
+    }),
+    checkOrderStatus: jest.fn(() => {
+      return 'CONFIRMED';
+    }),
+    cancelOrder: jest.fn(() => {
+      return {
+        id: 121323094948,
+        userId: '012220540493',
+        productName: 'RON96',
+        productPrice: 10,
+        productQuantity: 5,
+        totalCost: '50',
+        orderState: OrderStatusI.CANCELLED,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+      };
+    }),
   };
 
   beforeAll(async () => {
@@ -61,16 +105,58 @@ describe('AppController', () => {
       });
     });
   });
-  // describe('findAllOrder', () => {
-  //   it('should return all created Orders', async () => {
-  //     expect(await controller.findAllOrder()).toBeInstanceOf(Array);
-  //   });
-  // });
-  // describe('findOneOrder', () => {
-  //   it('should return one created Orders', async () => {
-  //     expect(await controller.findOneOrder({ id: orderId })).toBeInstanceOf(
-  //       OrderDto,
-  //     );
-  //   });
-  // });
+  describe('findAllOrder', () => {
+    it('should return an array of Orders', async () => {
+      expect(await controller.findAllOrder()).toEqual([
+        {
+          id: expect.any(Number),
+          userId: '012220540493',
+          productName: expect.any(String),
+          productPrice: 10,
+          productQuantity: 5,
+          totalCost: '50',
+          orderState: OrderStatusI.CREATED,
+          createdAt: expect.any(Number),
+          updatedAt: expect.any(Number),
+        },
+      ]);
+    });
+  });
+  describe('findOneOrder', () => {
+    it('should return one Order', async () => {
+      expect(await controller.findOneOrder({ id: '120122121313' })).toEqual({
+        id: 120122121313,
+        userId: '012220540493',
+        productName: expect.any(String),
+        productPrice: 10,
+        productQuantity: 5,
+        totalCost: '50',
+        orderState: OrderStatusI.CREATED,
+        createdAt: expect.any(Number),
+        updatedAt: expect.any(Number),
+      });
+    });
+  });
+  describe('checkOrderStatus', () => {
+    it('should return status for one Order', async () => {
+      expect(await controller.checkOrderStatus({ id: '121323094948' })).toEqual(
+        'CONFIRMED',
+      );
+    });
+  });
+  describe('cancelOrder', () => {
+    it('should return CANCELLED status for one Order', async () => {
+      expect(await controller.cancelOrder({ id: '121323094948' })).toEqual({
+        id: 121323094948,
+        userId: '012220540493',
+        productName: expect.any(String),
+        productPrice: 10,
+        productQuantity: 5,
+        totalCost: '50',
+        orderState: OrderStatusI.CANCELLED,
+        createdAt: expect.any(Number),
+        updatedAt: expect.any(Number),
+      });
+    });
+  });
 });
